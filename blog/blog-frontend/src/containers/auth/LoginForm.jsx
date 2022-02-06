0df51b5d41c91b1944756aad2b6ae760cf9f49e1
JMHan <hjm6021@gmail.com>
2022-02-06 20:05:16 +0900

    login and register page

--- blog/blog-frontend/src/containers/auth/LoginForm.jsx
+++ blog/blog-frontend/src/containers/auth/LoginForm.jsx
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { useNavigate } from 'react-router-dom';
+import { check } from '../../modules/user';
 
 const LoginForm = () => {
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
+    const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
         form: auth.login,
+        auth: auth.auth,
+        authError: auth.authError,
+        user: user.user,
     }));
 
     const onChange = (e) => {
@@ -21,13 +28,34 @@ const LoginForm = () => {
     };
     const onSubmit = (e) => {
         e.preventDefault();
+        const { username, password } = form;
+        dispatch(login({ username, password }));
     };
 
     useEffect(() => {
         dispatch(initializeForm('login'));
     }, [dispatch]);
 
-    return <AuthForm type="login" form={form} onChange={onChange} onSubmit={onSubmit} />;
+    useEffect(() => {
+        if (authError) {
+            console.log('오류 발생');
+            console.log(authError);
+            setError('로그인 실패');
+            return;
+        }
+        if (auth) {
+            console.log('로그인 성공');
+            dispatch(check());
+        }
+    }, [auth, authError, dispatch]);
+
+    useEffect(() => {
+        if (user) {
+            navigate('/');
+        }
+    }, [navigate, user]);
+
+    return <AuthForm type="login" form={form} onChange={onChange} onSubmit={onSubmit} error={error} />;
 };
 
 export default LoginForm;
--- blog/blog-frontend/src/containers/auth/RegisterForm.jsx
+++ blog/blog-frontend/src/containers/auth/RegisterForm.jsx
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { changeField, initializeForm, register } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
+import { useNavigate } from 'react-router-dom';
 
 const RegisterForm = () => {
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
+    const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
         form: auth.register,
+        auth: auth.auth,
+        authError: auth.authError,
+        user: user.user,
     }));
 
     const onChange = (e) => {
@@ -19,15 +26,52 @@ const RegisterForm = () => {
             })
         );
     };
+
     const onSubmit = (e) => {
         e.preventDefault();
+        const { username, password, passwordConfirm } = form;
+        if ([username, password, passwordConfirm].includes('')) {
+            setError('빈 칸을 모두 입력하세요');
+            return;
+        }
+        if (password !== passwordConfirm) {
+            setError('비밀번호가 일치하지 않습니다.');
+            dispatch(changeField({ form: 'register', key: 'password', value: '' }));
+            dispatch(changeField({ form: 'register', key: 'passwordConfirm', value: '' }));
+            return;
+        }
+        dispatch(register({ username, password }));
     };
 
     useEffect(() => {
         dispatch(initializeForm('register'));
     }, [dispatch]);
 
-    return <AuthForm type="register" form={form} onChange={onChange} onSubmit={onSubmit} />;
+    useEffect(() => {
+        if (authError) {
+            if (authError.response.status === 409) {
+                setError('이미 존재하는 계정명입니다.');
+                return;
+            }
+            setError('회원가입 실패');
+            return;
+        }
+        if (auth) {
+            console.log('회원가입 성공');
+            console.log(auth);
+            dispatch(check());
+        }
+    }, [auth, authError, dispatch]);
+
+    useEffect(() => {
+        if (user) {
+            console.log('check API 성공');
+            console.log(user);
+            navigate('/');
+        }
+    }, [navigate, user]);
+
+    return <AuthForm type="register" form={form} onChange={onChange} onSubmit={onSubmit} error={error} />;
 };
 
 export default RegisterForm;
